c997f50b581ccd30e909eb5a3c3cb0adb65f04fc
Arkadiy <alibullterrier@gmail.com>
2023-08-11 06:16:03 +0300

    add case

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -6,7 +6,11 @@ export type RemoveTodolistActionType = {
     type: 'REMOVE-TODOLIST',
     id: string
 }
-
+export type AddTodolistActionType = {
+    type: 'ADD-TODOLIST',
+    title: string
+    todolistId: string
+}
 
 
 
