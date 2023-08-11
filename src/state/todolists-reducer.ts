986a584782ec243ecda31d211905f898201b5039
Arkadiy <alibullterrier@gmail.com>
2023-08-11 06:14:25 +0300

    rm cases

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -6,11 +6,7 @@ export type RemoveTodolistActionType = {
     type: 'REMOVE-TODOLIST',
     id: string
 }
-export type AddTodolistActionType = {
-    type: 'ADD-TODOLIST',
-    title: string
-    todolistId: string
-}
+
 
 
 
