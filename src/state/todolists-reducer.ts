c8eccdec66ade20d2e505d8587b66865a1de748c
Arkadiy <alibullterrier@gmail.com>
2023-08-13 11:32:53 +0300

    new creator

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -84,7 +84,9 @@ export const addTodolistAC = (title: string): AddTodolistActionType => {
 export const changeTodolistTitleAC = (id: string, title: string): ChangeTodolistTitleActionType => {
     return {type: 'CHANGE-TODOLIST-TITLE', id: id, title: title}
 }
-
+export const changeTodolistFilterAC = (id: string, filter: FilterValuesType): ChangeTodolistFilterActionType => {
+    return {type: 'CHANGE-TODOLIST-FILTER', id: id, filter: filter}
+}
 
 export type SetTodolistsType = ReturnType<typeof setTodolistAC>
 
