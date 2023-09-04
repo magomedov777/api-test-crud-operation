e44876fdb9c23ead12047b58900cbc30a7b926b9
Arkadiy <alibullterrier@gmail.com>
2023-09-04 06:16:54 +0300

    create: new case, addTodolist

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -50,6 +50,18 @@ export const todolistsReducer = (
     case "REMOVE-TODOLIST": {
       return state.filter((tl) => tl.id !== action.id);
     }
+    case "ADD-TODOLIST": {
+      return [
+        {
+          id: action.todolistId,
+          title: action.title,
+          filter: "all",
+          addedDate: "",
+          order: 0,
+        },
+        ...state,
+      ];
+    }
 
     default:
       return state;
