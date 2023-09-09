e5104f5974eb676cfe09306e999db1c47241369d
Arkadiy <alibullterrier@gmail.com>
2023-09-09 08:14:24 +0300

    create: rm task

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,9 @@ function App() {
         dispatch(getTodosTC())
     }, [])
 
-
+    const removeTask = useCallback(function (id: string, todolistId: string) {
+        dispatch(deleteTasksTC(todolistId, id));
+    }, []);
 
     const addTask = useCallback(function (title: string, todolistId: string) {
         dispatch(createTaskTC(todolistId, title));
