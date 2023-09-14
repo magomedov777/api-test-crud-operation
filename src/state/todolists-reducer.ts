fd9adedae20ba6f36cdce2d5e3202e815728cc99
Arkadiy <alibullterrier@gmail.com>
2023-09-14 06:02:05 +0300

    create: todolist reducer, rewrite

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -39,6 +39,50 @@ export type TodolistDomainType = TodolistType & {
   filter: FilterValuesType;
 };
 
+export const todolistsReducer = (
+  state: Array<TodolistDomainType> = initialState,
+  action: ActionsType
+): Array<TodolistDomainType> => {
+  switch (action.type) {
+    case "SET-TODOS": {
+      return action.todos.map((tl) => ({ ...tl, filter: "all" }));
+    }
+    case "REMOVE-TODOLIST": {
+      return state.filter((tl) => tl.id !== action.id);
+    }
+    case "ADD-TODOLIST": {
+      return [
+        {
+          id: action.todolistId,
+          title: action.title,
+          filter: "all",
+          addedDate: "",
+          order: 0,
+        },
+        ...state,
+      ];
+    }
+    case "CHANGE-TODOLIST-TITLE": {
+      const todolist = state.find((tl) => tl.id === action.id);
+      if (todolist) {
+        // если нашёлся - изменим ему заголовок
+        todolist.title = action.title;
+      }
+      return [...state];
+    }
+    case "CHANGE-TODOLIST-FILTER": {
+      const todolist = state.find((tl) => tl.id === action.id);
+      if (todolist) {
+        // если нашёлся - изменим ему заголовок
+        todolist.filter = action.filter;
+      }
+      return [...state];
+    }
+    default:
+      return state;
+  }
+};
+
 export const removeTodolistAC = (todolistId: string): RemoveTodolistActionType => {
   return { type: "REMOVE-TODOLIST", id: todolistId };
 };
