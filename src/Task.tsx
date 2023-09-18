394ea5c80bb2c8350f3dd24109aa928b6657e13a
Arkadiy <alibullterrier@gmail.com>
2023-09-18 06:10:38 +0300

    create new on change handler

--- src/Task.tsx
+++ src/Task.tsx
@@ -9,7 +9,10 @@ import { TaskStatuses, TaskType } from './api/todolists-api'
 export const Task = React.memo((props: TaskPropsType) => {
     const onClickHandler = useCallback(() => props.removeTask(props.task.id, props.todolistId), [props.task.id, props.todolistId]);
 
-
+    const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        let newIsDoneValue = e.currentTarget.checked
+        props.changeTaskStatus(props.task.id, newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New, props.todolistId)
+    }, [props.task.id, props.todolistId]);
 
     const onTitleChangeHandler = useCallback((newValue: string) => {
         props.changeTaskTitle(props.task.id, newValue, props.todolistId)
