dbf422ec75a8a9c2e4e1ddac383a53fc8b47bf5c
Arkadiy <alibullterrier@gmail.com>
2023-09-27 05:55:11 +0300

    create: change todolist title case

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -61,7 +61,14 @@ export const todolistsReducer = (
         ...state,
       ];
     }
-
+    case "CHANGE-TODOLIST-TITLE": {
+      const todolist = state.find((tl) => tl.id === action.id);
+      if (todolist) {
+        // если нашёлся - изменим ему заголовок
+        todolist.title = action.title;
+      }
+      return [...state];
+    }
     case "CHANGE-TODOLIST-FILTER": {
       const todolist = state.find((tl) => tl.id === action.id);
       if (todolist) {
