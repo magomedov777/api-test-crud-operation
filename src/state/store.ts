85aab85781c6472235f9e212f09ad9dac1adc29d
Arkadiy <alibullterrier@gmail.com>
2023-10-14 00:02:55 +0300

    remove old comm, clean

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,27 +1,21 @@
-import thunk, { ThunkDispatch } from 'redux-thunk';
-import { tasksReducer } from './tasks-reducer';
-import { todolistsReducer } from './todolists-reducer';
-import {combineReducers, AnyAction, createStore, legacy_createStore, applyMiddleware} from 'redux';
-import { TypedUseSelectorHook, useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
+import thunk, { ThunkDispatch } from "redux-thunk";
+import { tasksReducer } from "./tasks-reducer";
+import { todolistsReducer } from "./todolists-reducer";
+import { combineReducers, AnyAction, createStore, legacy_createStore, applyMiddleware } from "redux";
+import { TypedUseSelectorHook, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer
-})
-// непосредственно создаём store
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+});
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
+export type AppDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction>;
 
-export type AppDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction> 
+export const useAppDispatch = () => useDispatch<AppDispatchType>();
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
+export type AppRootStateType = ReturnType<typeof rootReducer>;
 
-export const useAppDispatch = () => useDispatch<AppDispatchType>()
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
-// определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
-
-// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
